db28dee774950422bef95ebdf5fec8330bfb1408
Edoardo Serra <12883861+serraedoardo@users.noreply.github.com>
2023-07-11 15:44:13 +0200

    symmetries.py

--- symmetries.py
+++ symmetries.py
@@ -19,19 +19,33 @@ def returnValue(x_t, tPy, t, tNum):
         return x_t.subs(t, tNum)
     return x_t
 
+def returnValueNoAss(x_t, t, tNum):
+    if isinstance(x_t, sp.Basic):
+        return x_t.subs(t, tNum)
+    return x_t
+
 def curve(x_t, y_t, tPy, t, tNum):
     if notReal(x_t, tPy, t, tNum) or notReal(y_t, tPy, t, tNum):
         return 0, 0, False
     return returnValue(x_t, tPy, t, tNum), returnValue(y_t, tPy, t, tNum), True
 
-def getCoeffPerp(xPrime_t, yPrime_t, tPy, t, tNum):
-    yPrime = returnValue(yPrime_t, tPy, t, tNum)
-    xPrime = returnValue(xPrime_t, tPy, t, tNum)
-    if yPrime != 0 and xPrime != 0:
+def getCoeffPerp(x_t, y_t, xPrime_t1, yPrime_t1, tPy, t, t1, tNum):
+    yPrime = returnValueNoAss(yPrime_t1, t1, tNum)
+    xPrime = returnValueNoAss(xPrime_t1, t1, tNum)
+    if not (yPrime == sp.sympify("nan") or yPrime == sp.sympify("+oo") or yPrime == sp.sympify("-oo")): #derivative at tNum is a real number. tNun is always real so no chanche of having a derivative with non-zero immaginary part
+        yPrime_t = sp.diff(y_t, t) if isinstance(y_t, sp.Basic) else 0 #calculate the derivative with the real=True assumption, such that it returns just a number
+        yPrime = returnValue(yPrime_t, tPy, t, tNum)
+    if not (xPrime == sp.sympify("nan") or xPrime == sp.sympify("+oo") or xPrime == sp.sympify("-oo")):
+        xPrime_t = sp.diff(x_t, t) if isinstance(x_t, sp.Basic) else 0
+        xPrime = returnValue(xPrime_t, tPy, t, tNum)
+    
+    if yPrime == sp.sympify("nan") or xPrime == sp.sympify("nan"):
+        return np.nan
+    elif yPrime != 0 and xPrime != 0: #they can't be both inf so no worries about that
         return -xPrime/yPrime #-1/coeff tan
     elif xPrime == 0:
         return 0
-    else:
+    else: #yPrime == 0
         return np.inf
 
 def segment(xToBeMirrored_t, yToBeMirrored_t, tPy, t, tRange):
@@ -67,10 +81,10 @@ def intersect(xMirror, yMirror, coeff, xSegmentList, ySegmentList, qNum_1List, q
             intersection = sp.linsolve([xSegmentList[i]-xLine_r, ySegmentList[i]-yLine_r], q, r)
             qNum, rNum = list(intersection)[0] #if the same, qNum is symbolical. If without intersections, length is 0
             if r in qNum.free_symbols or q in qNum.free_symbols:
-                coin, toBeMirrored_1Tupla, toBeMirrored_2Tupla = coincident(xMirror_t, yMirror_t, t, tNum, xToBeMirrored_q, yToBeMirrored_q, q, qNum_1List[i], qNum_2List[i], coeff, tPy, qPy)
+                coin, toBeMirrored_1Tuple, toBeMirrored_2Tuple = coincident(xMirror_t, yMirror_t, t, tNum, xToBeMirrored_q, yToBeMirrored_q, q, qNum_1List[i], qNum_2List[i], coeff, tPy, qPy)
                 if coin:
-                    intersections += [toBeMirrored_1Tupla]
-                    intersections += [toBeMirrored_2Tupla]
+                    intersections += [toBeMirrored_1Tuple]
+                    intersections += [toBeMirrored_2Tuple]
             elif 0 <= qNum <= 1:
                 intersections += [(xSegmentList[i].subs(q, qNum), ySegmentList[i].subs(q, qNum))]
         except ValueError:
@@ -78,10 +92,12 @@ def intersect(xMirror, yMirror, coeff, xSegmentList, ySegmentList, qNum_1List, q
         except IndexError:
             #probably just a continue would do
             if len(list(intersection)) != 0:
-                coin, toBeMirrored_1Tupla, toBeMirrored_2Tupla = coincident(xMirror_t, yMirror_t, t, tNum, xToBeMirrored_q, yToBeMirrored_q, q, qNum_1List[i], qNum_2List[i], coeff, tPy, qPy)
+                coin, toBeMirrored_1Tuple, toBeMirrored_2Tuple = coincident(xMirror_t, yMirror_t, t, tNum, xToBeMirrored_q, yToBeMirrored_q, q, qNum_1List[i], qNum_2List[i], coeff, tPy, qPy)
                 if coin:
-                    intersections += [toBeMirrored_1Tupla]
-                    intersections += [toBeMirrored_2Tupla]
+                    intersections += [toBeMirrored_1Tuple]
+                    intersections += [toBeMirrored_2Tuple]
+        except Exception:
+            continue
     return intersections
 
 def linIndip(xMirror, yMirror, xToBeMirrored_1, yToBeMirrored_1, xToBeMirrored_2, yToBeMirrored_2):
@@ -110,14 +126,21 @@ def coincident(xMirror_t, yMirror_t, t, tNum, xToBeMirrored_q, yToBeMirrored_q, 
 
 def mirror(xSegmentList, ySegmentList, qNum_1List, qNum_2List, xMirror_t, yMirror_t, xToBeMirrored_q, yToBeMirrored_q, t, tRange, tPy, qPy, currentProcess, nProcesses, mirroredShared):
     tRange = islice(tRange, currentProcess, len(tRange), nProcesses)
-    xPrime_t = sp.diff(xMirror_t, t) if isinstance(xMirror_t, sp.Basic) else 0
-    yPrime_t = sp.diff(yMirror_t, t) if isinstance(yMirror_t, sp.Basic) else 0
+    t1 = sp.Symbol('t')
+    xMirror_t1, yMirror_t1 = xMirror_t.subs(t, t1), yMirror_t.subs(t, t1)
+    xPrime_t1 = sp.diff(xMirror_t1, t1) if isinstance(xMirror_t1, sp.Basic) else 0 #evaluating complex derivatives to avoid sympy inaccuracies due to assumptions (eg. sign(0) = 0 instead of nan)
+    yPrime_t1 = sp.diff(yMirror_t1, t1) if isinstance(yMirror_t1, sp.Basic) else 0
     for tNum in tRange:
         print(tNum)
         xMirror, yMirror, real = curve(xMirror_t, yMirror_t, tPy, t, tNum)
         if not real:
             continue
-        coeff = getCoeffPerp(xPrime_t, yPrime_t, tPy, t, tNum)
+        coeff = getCoeffPerp(xMirror_t, yMirror_t, xPrime_t1, yPrime_t1, tPy, t, t1, tNum)
+        match coeff:
+            case np.nan:
+                continue
+            case _:
+                pass
         intersections = intersect(xMirror, yMirror, coeff, xSegmentList, ySegmentList, qNum_1List, qNum_2List, tNum, xMirror_t, yMirror_t, xToBeMirrored_q, yToBeMirrored_q, tPy, qPy)
         for i in intersections:
             mirroredShared.append(calcSimm(xMirror, yMirror, i[0], i[1]))
